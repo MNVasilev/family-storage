a0476f983e3bb002e3b89f4c89c287e6781d49b1
vasilev <mv@smartlombard.ru>
2020-02-22 11:26:04 +0300

    Страница входа

---
+++ resources/lang/en/family.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: VMichael
+ * Date: 22.02.2020
+ * Time: 10:56
+ */
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'f_FamStor' => 'Family story',
+    'Login' => 'Login'
+];
