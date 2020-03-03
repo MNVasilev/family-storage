477b4e7fd2bf0afad34deece533997be52c1daea
vasilev <mv@smartlombard.ru>
2020-03-03 10:49:24 +0300

    Формирование меню

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,12 +24,17 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ __('Семейное хранилище') }}
+                    {{ __('Главная') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ __('Книги') }}
+                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ __('Фильмы') }}
+                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ __('Администрирование') }}
+                </a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
